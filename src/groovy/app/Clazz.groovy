d865b04bb6ec2ebb2cc8bf37078062126cdbcf55
Stephen Hendrix <hendrix@shendrix.com>
2014-08-23 17:45:43 -0500

    Loading ontology data

---
+++ src/groovy/app/Clazz.groovy
@@ -0,0 +1,14 @@
+package app
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: steve
+ * Date: 8/23/14
+ * Time: 5:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Clazz {
+    String label
+
+    List<Clazz> children
+}
